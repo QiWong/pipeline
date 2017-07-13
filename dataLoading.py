df1ea07589a633990842e41d2e2157f87b815fa9
QiWong <qiwong@QiWongdeMacBook-Pro.local>
2017-07-13 14:52:32 -0700

    Updata data loading module'

--- dataLoading.py
+++ dataLoading.py
@@ -4,22 +4,24 @@
 import pandas as pd
 from pandas import ExcelFile
 
-def readFile(file_name):
+def readFile(file_name, index_column):
 	#Read the raw data from the source file
 	#The file could be csv format or xls format
 	#The first line should be the header
+	#Usually, one column of the dataset is the index column, not the feature, we will not use this column in our prediction.
 	raw_dataset = None
 	if file_name.endswith('.xls'):
-		raw_dataset = pd.read_excel(file_name)
+		raw_dataset = pd.read_excel(file_name, index_col= index_column)
 		#Read the dataset from the first sheet of xls file
 	elif file_name.endswith('.csv'):
-		raw_dataset = pd.read_csv(file_name)
+		raw_dataset = pd.read_csv(file_name, index_col= index_column)
 	#raw_dataset is in pandas DataFrame type
 	return raw_dataset
 
-def dataSelection(raw_dataset):
-	#Choose useful columns in dataset. The raw_dataset is in pandas DataFrame type.
+def dropColumns(raw_dataset, column_name_list):
+	#Delete columns in dataset, because some features like Date we probably don't want to use it in learning phase. The raw_dataset is in pandas DataFrame type.
+	#Delete column by name
+	for column_name in column_name_list:
+		del raw_dataset[column_name]
+	return raw_dataset
 
-
-def handleProblematicData(raw_dataset):
-	
---
+++ mainWorkFlow.py
@@ -0,0 +1,35 @@
+#This is main work flow file in this pipeline. It will call other modules to do
+#the data processing.
+#!/usr/bin/python
+
+import dataLoading
+import sys
+
+#file_name is the name of the file which stores the dataset
+class  PipeLine:
+	def __init__(self, file_name):
+		#We assume the first column of the dataset is the index column, which will not be used in our learning phase.
+		self.data_frame = dataLoading.readFile(file_name, 0)
+
+	def dropColumns(self, col_name_list):
+		self.data_frame = dataLoading.dropColumns(self.data_frame, col_name_list)
+
+def main(argv):
+	if not argv:
+		#Temporarily we use mpMRI_FeatureList_CQ.csv
+		#print "The file name is empty";
+		argv.append('mpMRI_FeatureList_CQ.csv')
+		pipeline = PipeLine(argv[0])
+		
+		'''There are five columns in the mpMRI_FeatureList_CQ dataset that we won't use in our learning phase.
+		Then we delete these five columns from the dataset'''
+		unused_columns = ['StudyDate','PSA','Gleason','Location','PIRADS']
+		pipeline.dropColumns(unused_columns)
+		
+	else:
+		print "The file name is "+argv[0]
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+	pass
+
